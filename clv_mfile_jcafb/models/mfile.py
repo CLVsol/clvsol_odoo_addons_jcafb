d0153919bf812ed0baf96c2e30eeaf1f817479c1
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-06-07 15:01:40 +0000

    [IMP] clv_mfile_jcafb (imp mfile fields)

--- clv_mfile_jcafb/models/mfile.py
+++ clv_mfile_jcafb/models/mfile.py
@@ -40,34 +40,47 @@ class Address(models.Model):
         string='Survey User Input'
     )
 
-    document_code = fields.Char(string='Document Code')
+    # document_code = fields.Char(string='Document Code')
     document_id = fields.Many2one(
         comodel_name='clv.document',
         string='Related Document')
     document_state = fields.Selection(
-        string='Document Status',
+        string='Document State',
         related='document_id.state',
         store=False,
         readonly=True
     )
 
-    person_code = fields.Char(string="Person Code")
+    # person_code = fields.Char(string="Person Code")
     person_id = fields.Many2one(
         comodel_name='clv.person',
         string='Related Person'
     )
+    person_state = fields.Selection(
+        string='Person State',
+        related='person_id.state',
+        store=False,
+        readonly=True
+    )
 
-    address_code = fields.Char(help="Address Code")
+    # address_code = fields.Char(help="Address Code")
     address_id = fields.Many2one(
         comodel_name='clv.address',
         string="Related Address"
     )
+    address_state = fields.Selection(
+        string='Address State',
+        related='address_id.state',
+        store=False,
+        readonly=True
+    )
 
-    lab_test_request_code = fields.Char(string="Lab Test Request Code")
+    # lab_test_request_code = fields.Char(string="Lab Test Request Code")
     # lab_test_request_id = fields.Many2one(
     #     comodel_name='clv.lab_test.request',
     #     string="Related Lab Test Request"
     # )
+
     user_id = fields.Many2one(
         comodel_name='res.users',
         string='Document Responsible',
