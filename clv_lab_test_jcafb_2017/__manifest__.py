2df19b57be693134b557e52a740a56641e7d5292
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-08-06 15:12:18 +0000

    [ADD] clv_lab_test_jcafb_2017

---
+++ clv_lab_test_jcafb_2017/__manifest__.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'JCAFB Lab Tests (2017)',
+    'summary': 'This module will install all the JCAFB lab tests (2017).',
+    'version': '3.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_lab_test_jcafb',
+    ],
+    'data': [
+        'data/lab_test_EAN17_data.xml',
+        'data/lab_test_EDH17_data.xml',
+        'data/lab_test_EPC17_data.xml',
+        'data/lab_test_EPI17_data.xml',
+        'data/lab_test_ECP17_data.xml',
+        'data/lab_test_EEV17_data.xml',
+        'data/lab_test_EUR17_data.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
