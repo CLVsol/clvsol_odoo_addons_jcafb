6fb020f86c9a7020ff55c4710a10fb5e96d18e57
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-10-15 00:43:33 +0000

    [ADD] clv_default_jcafb_2018

---
+++ clv_default_jcafb_2018/README.rst
@@ -0,0 +1,11 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+===============================
+Default Parameters (JCAFB-2018)
+===============================
+
+Default Parameters for CLVhealth-JCAFB Solution (2018).
+
+Developed by `CLVsol <https://github.com/CLVsol>`_.
---
+++ clv_default_jcafb_2018/wizard/person_mng_person_create.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from odoo import fields, models
+
+
+class PersonMngPersonCreate(models.TransientModel):
+    _inherit = 'clv.person.mng.person_create'
+
+    def _default_history_marker_id(self):
+        HistoryMarker = self.env['clv.history_marker']
+        history_marker = HistoryMarker.search([
+            ('name', '=', 'JCAFB-2018'),
+        ])
+        history_marker_id = False
+        if history_marker.id is not False:
+            history_marker_id = history_marker.id
+        return history_marker_id
+    history_marker_id = fields.Many2one(
+        comodel_name='clv.history_marker',
+        string='History Marker',
+        ondelete='restrict',
+        default=_default_history_marker_id
+    )
