dc517866f46a3ca9004eef1557c0d1f35e28ad6e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-11-08 16:14:56 +0000

    [IMP] clv_lab_test_jcafb_2018 (IMP wizard/lab_test_report_edit_EEV18)

--- clv_lab_test_jcafb_2018/wizard/lab_test_report_edit_EEV18.py
+++ clv_lab_test_jcafb_2018/wizard/lab_test_report_edit_EEV18.py
@@ -41,58 +41,16 @@ class LabTestReportEdit(models.TransientModel):
         return is_EEV18
     is_EEV18 = fields.Boolean('Is EEV18', readonly=True, default=_default_is_EEV18)
 
-    def _default_EEV18_data_entrada_material(self):
-        return self._get_default('EEV18', 'EEV18-01-01')
-    EEV18_data_entrada_material = fields.Date(
-        'Data de Entrada do Material', readonly=False, default=_default_EEV18_data_entrada_material
-    )
-
-    def _write_EEV18_data_entrada_material(self):
-        self._set_result('EEV18', 'EEV18-01-01', self.EEV18_data_entrada_material)
-
-    def _default_EEV18_liberacao_reportado(self):
-        return self._get_default('EEV18', 'EEV18-01-02')
-    EEV18_liberacao_reportado = fields.Date(
-        'Liberação do Reportado', readonly=False, default=_default_EEV18_liberacao_reportado
-    )
-
-    def _write_EEV18_liberacao_reportado(self):
-        self._set_result('EEV18', 'EEV18-01-02', self.EEV18_liberacao_reportado)
-
-    def _default_EEV18_reportado(self):
+    def _default_EEV18_resultado(self):
         return self._get_default('EEV18', 'EEV18-01-03')
-    EEV18_reportado = fields.Selection([
+    EEV18_resultado = fields.Selection([
         ('Positivo', 'Positivo'),
         ('NEGATIVO', 'NEGATIVO'),
         ('Não Realizado', 'Não Realizado'),
-    ], 'Reportado', readonly=False, default=_default_EEV18_reportado)
+    ], 'Resultado', readonly=False, default=_default_EEV18_resultado)
 
-    def _write_EEV18_reportado(self):
-        self._set_result('EEV18', 'EEV18-01-03', self.EEV18_reportado)
-
-    def _default_EEV18_examinador(self):
-        employee_model = self.env['hr.employee']
-        code = self._get_default('EEV18', 'EEV18-01-04')
-        if code is not False:
-            code = code[code.find('[') + 1:code.find(']')]
-            employee_search = employee_model.search([
-                ('code', '=', code),
-            ])
-            return employee_search
-        else:
-            return False
-    EEV18_examinador = fields.Many2one(
-        'hr.employee',
-        string='Examinador',
-        readonly=False,
-        default=_default_EEV18_examinador
-    )
-
-    def _write_EEV18_examinador(self):
-        self._set_result(
-            'EEV18', 'EEV18-01-04',
-            self.EEV18_examinador.name + ' [' + self.EEV18_examinador.code + ']'
-        )
+    def _write_EEV18_resultado(self):
+        self._set_result('EEV18', 'EEV18-01-03', self.EEV18_resultado)
 
     def _default_EEV18_metodo_utilizado(self):
         return self._get_default('EEV18', 'EEV18-01-05')
@@ -115,10 +73,7 @@ class LabTestReportEdit(models.TransientModel):
 
     def _do_report_updt_EEV18(self):
 
-        self._write_EEV18_data_entrada_material()
-        self._write_EEV18_liberacao_reportado()
-        self._write_EEV18_reportado()
-        self._write_EEV18_examinador()
+        self._write_EEV18_resultado()
         self._write_EEV18_metodo_utilizado()
         self._write_EEV18_obs()
 
