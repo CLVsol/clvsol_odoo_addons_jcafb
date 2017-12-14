23ab3025ba6dab05fb5a8130e17d1d55ab752422
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-12-14 14:53:35 -0200

    [IMP] clv_default_jcafb_2018 (ADD views/document_view.xml)

--- clv_default_jcafb_2018/__manifest__.py
+++ clv_default_jcafb_2018/__manifest__.py
@@ -37,6 +37,7 @@
     'data': [
         'views/address_view.xml',
         'views/person_view.xml',
+        'views/document_view.xml',
         'wizard/person_select_2018_view.xml',
     ],
     'demo': [],
