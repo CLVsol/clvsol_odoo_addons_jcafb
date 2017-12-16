f587479ebc2faf7013076730568badcce49bd92b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-12-16 18:32:39 -0200

    [IMP] clv_lab_test_jcafb (ADD wizard/lab_test_result_approve)

--- clv_lab_test_jcafb/__manifest__.py
+++ clv_lab_test_jcafb/__manifest__.py
@@ -65,6 +65,7 @@
         'wizard/lab_test_report_edit_view.xml',
         'wizard/lab_test_result_copy_to_report_view.xml',
         'wizard/lab_test_request_direct_mail_setup_view.xml',
+        'wizard/lab_test_result_approve_view.xml',
     ],
     'demo': [],
     'test': [],
--- clv_lab_test_jcafb/wizard/__init__.py
+++ clv_lab_test_jcafb/wizard/__init__.py
@@ -27,3 +27,4 @@ import lab_test_result_edit
 import lab_test_report_edit
 import lab_test_request_direct_mail_setup
 import lab_test_result_copy_to_report
+import lab_test_result_approve
