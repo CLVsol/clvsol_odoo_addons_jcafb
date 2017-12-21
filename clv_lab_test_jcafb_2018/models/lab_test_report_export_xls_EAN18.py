c28c33e9e56f47ed12e440aa6d95c24f85ddda28
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-12-21 21:06:00 -0200

    [IMP] clv_lab_test_jcafb_2018 (ADD models/lab_test_report_export_xls_EAN18.py)

--- clv_lab_test_jcafb_2018/models/lab_test_report_export_xls_EAN18.py
+++ clv_lab_test_jcafb_2018/models/lab_test_report_export_xls_EAN18.py
@@ -18,9 +18,35 @@
 #
 ###############################################################################
 
-from . import lab_test_result_export_xls_ECP18
-from . import lab_test_result_export_xls_EEV18
-from . import lab_test_result_export_xls_EUR18
-from . import lab_test_result_export_xls
-from . import lab_test_report_export_xls_EAN18
-from . import lab_test_report_export_xls
+import logging
+# import xlwt
+
+from odoo import models
+
+_logger = logging.getLogger(__name__)
+
+
+class LabTestReport(models.Model):
+    _name = "clv.lab_test.report"
+    _inherit = 'clv.lab_test.report'
+
+    def lab_test_report_export_xls_EAN18(self, sheet, row_nr):
+
+        lab_test_type = self.lab_test_type_id.code
+        lab_test_request_code = self.lab_test_request_id.code
+
+        row_nr += 1
+        row = sheet.row(row_nr)
+        row.write(0, 'Person:')
+        row.write(3, self.person_id.name)
+        row_nr += 1
+        row = sheet.row(row_nr)
+        row.write(0, 'Lab Test Type:')
+        row.write(3, lab_test_type)
+        row_nr += 1
+        row = sheet.row(row_nr)
+        row.write(0, 'Request Code:')
+        row.write(3, lab_test_request_code)
+        row_nr += 1
+
+        return True
