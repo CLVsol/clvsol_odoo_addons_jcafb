f4d1e62f9a2facc4d53620db7001c6b6b589f8f2
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2017-12-27 19:18:25 -0200

    [IMP] clv_survey_jcafb_2018 (ADD data/document_jcafb_TID18.xml)

--- clv_survey_jcafb_2018/__manifest__.py
+++ clv_survey_jcafb_2018/__manifest__.py
@@ -51,6 +51,7 @@
         'data/document_jcafb_TAN18.xml',
         'data/document_jcafb_TCR18.xml',
         'data/document_jcafb_TDH18.xml',
+        'data/document_jcafb_TID18.xml',
 
         'wizard/document_item_edit_TAN18_view.xml',
         'wizard/document_item_edit_TCR18_view.xml',
