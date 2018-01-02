d97c3b35c3b6a8c32e1776e1cd3d0d5cadc78897
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-01-02 11:05:44 -0200

    [IMP] clv_lab_test_jcafb_2018 (IMP models/lab_test_result_export_xls_EUR18.py)

--- clv_lab_test_jcafb_2018/models/lab_test_result_export_xls_EUR18.py
+++ clv_lab_test_jcafb_2018/models/lab_test_result_export_xls_EUR18.py
@@ -30,7 +30,7 @@ class LabTestResult(models.Model):
     _name = "clv.lab_test.result"
     _inherit = 'clv.lab_test.result'
 
-    def lab_test_result_export_xls_EUR18(self, sheet, row_nr):
+    def lab_test_result_export_xls_EUR18(self, sheet, row_nr, use_template):
 
         ExportXLS = self.env['clv.export_xls']
 
@@ -38,143 +38,295 @@ class LabTestResult(models.Model):
         lab_test_request_code = self.lab_test_request_id.code
         lab_test_result_code = self.code
 
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'JCAFB-2018 - FERNÃO - SP')
-        row_nr += 1
-        ExportXLS.setOutCell(sheet, 0, row_nr, lab_test_type + ' - ' + u'URINA TIPO I - RESULTADO')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Código da Requisição:')
-        ExportXLS.setOutCell(sheet, 10, row_nr, lab_test_request_code)
-        ExportXLS.setOutCell(sheet, 25, row_nr, u'Código do Resultado:')
-        ExportXLS.setOutCell(sheet, 35, row_nr, lab_test_result_code)
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Nome da Pessoa:')
-        ExportXLS.setOutCell(sheet, 10, row_nr, self.person_id.name)
-        row_nr += 1
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Código da Pessoa:')
-        ExportXLS.setOutCell(sheet, 10, row_nr, self.person_id.code)
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Recebido por:')
-        ExportXLS.setOutCell(sheet, 10, row_nr, self.lab_test_request_id.employee_id.name)
-        row_nr += 1
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Código do Recebedor:')
-        ExportXLS.setOutCell(sheet, 10, row_nr, self.lab_test_request_id.employee_id.code)
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Data do Exame:')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Resultado:')
-        row_nr += 2
-
-        ref_row_nr = row_nr
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Exame Físico')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Volume (mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Densidade:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Aspecto:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Cor:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Odor:')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Exame Químico (Tiras Reagentes)')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'pH:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Proteínas:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Glicose:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Cetona:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Pigmentos biliares:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Sangue:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Urobilinogênio:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Nitrito:')
-        row_nr += 4
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Analisador(a):')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Observações:')
-        row_nr += 3
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Revisado por:')
-        ExportXLS.setOutCell(sheet, 25, row_nr, u'Data:')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Resultado Confirmado (   )')
-        ExportXLS.setOutCell(sheet, 25, row_nr, u'Resultado Alterado (   )')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, 0, row_nr, u'Resultado digitado em:')
-        ExportXLS.setOutCell(sheet, 25, row_nr, u'Digitado por:')
-        row_nr += 2
-
-        row_nr = ref_row_nr
-        delta = 25
-
-        ExportXLS.setOutCell(sheet, delta, row_nr, u'Exame Microscópico')
-        row_nr += 2
-
-        ExportXLS.setOutCell(sheet, delta, row_nr, u'Células epiteliais:')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta, row_nr, u'Muco (filamentos):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta, row_nr, u'Cristais:')
-        row_nr += 4
-
-        ExportXLS.setOutCell(sheet, delta, row_nr, u'Leucócitos (/mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta, row_nr, u'Hemácias (/mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta, row_nr, u'Cilindros (/mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Hialinos (/mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Granulosos (/mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Leucocitários (/mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Hemáticos (/mL):')
-        row_nr += 1
-
-        ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Outros (/mL):')
-        row_nr += 1
+        sheet.header_str = ''
+        sheet.footer_str = ''
 
         for i in range(0, 49):
             sheet.col(i).width = 256 * 2
         sheet.show_grid = False
+
+        if use_template:
+
+            # ExportXLS.setOutCell(sheet, 17, row_nr, u'JCAFB-2018 - FERNÃO - SP')
+            row_nr += 1
+            # ExportXLS.setOutCell(sheet, 15, row_nr, lab_test_type + ' - ' + u'URINA TIPO I - RESULTADO')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Código da Requisição:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, lab_test_request_code)
+            # ExportXLS.setOutCell(sheet, 33, row_nr, u'Código do Resultado:')
+            ExportXLS.setOutCell(sheet, 43, row_nr, lab_test_result_code)
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Nome da Pessoa:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.person_id.name)
+            row_nr += 1
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Código da Pessoa:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.person_id.code)
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Recebido por:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.lab_test_request_id.employee_id.name)
+            row_nr += 1
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Código do Recebedor:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.lab_test_request_id.employee_id.code)
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Data do Exame:')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 20, row_nr, u'Resultado')
+            row_nr += 2
+
+            ref_row_nr = row_nr
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Exame Físico')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Volume (mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Densidade:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Aspecto:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Cor:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Odor:')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Exame Químico (Tiras Reagentes)')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'pH:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Proteínas:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Glicose:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Cetona:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Pigmentos biliares:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Sangue:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Urobilinogênio:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Nitrito:')
+            row_nr += 3
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Observações:')
+            row_nr += 4
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Analisador(a):')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Revisado por:')
+            # ExportXLS.setOutCell(sheet, 25, row_nr, u'Data:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Resultado Confirmado (   )')
+            # ExportXLS.setOutCell(sheet, 25, row_nr, u'Resultado Alterado (   )')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, 0, row_nr, u'Resultado digitado em:')
+            # ExportXLS.setOutCell(sheet, 25, row_nr, u'Digitado por:')
+            row_nr += 2
+
+            row = sheet.row(row_nr)
+            # for i in range(0, 49):
+            #     row.write(i, u'-')
+            row_nr += 2
+
+            row_nr = ref_row_nr
+            delta = 25
+
+            # ExportXLS.setOutCell(sheet, delta, row_nr, u'Exame Microscópico')
+            row_nr += 2
+
+            # ExportXLS.setOutCell(sheet, delta, row_nr, u'Células epiteliais:')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta, row_nr, u'Muco (filamentos):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta, row_nr, u'Cristais:')
+            row_nr += 4
+
+            # ExportXLS.setOutCell(sheet, delta, row_nr, u'Leucócitos (/mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta, row_nr, u'Hemácias (/mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta, row_nr, u'Cilindros (/mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Hialinos (/mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Granulosos (/mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Leucocitários (/mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Hemáticos (/mL):')
+            row_nr += 1
+
+            # ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Outros (/mL):')
+            row_nr += 1
+
+        else:
+
+            ExportXLS.setOutCell(sheet, 17, row_nr, u'JCAFB-2018 - FERNÃO - SP')
+            row_nr += 1
+            ExportXLS.setOutCell(sheet, 15, row_nr, lab_test_type + ' - ' + u'URINA TIPO I - RESULTADO')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Código da Requisição:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, lab_test_request_code)
+            ExportXLS.setOutCell(sheet, 33, row_nr, u'Código do Resultado:')
+            ExportXLS.setOutCell(sheet, 43, row_nr, lab_test_result_code)
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Nome da Pessoa:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.person_id.name)
+            row_nr += 1
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Código da Pessoa:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.person_id.code)
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Recebido por:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.lab_test_request_id.employee_id.name)
+            row_nr += 1
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Código do Recebedor:')
+            ExportXLS.setOutCell(sheet, 10, row_nr, self.lab_test_request_id.employee_id.code)
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Data do Exame:')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 20, row_nr, u'Resultado')
+            row_nr += 2
+
+            ref_row_nr = row_nr
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Exame Físico')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Volume (mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Densidade:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Aspecto:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Cor:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Odor:')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Exame Químico (Tiras Reagentes)')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'pH:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Proteínas:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Glicose:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Cetona:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Pigmentos biliares:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Sangue:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Urobilinogênio:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Nitrito:')
+            row_nr += 3
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Observações:')
+            row_nr += 4
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Analisador(a):')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Revisado por:')
+            ExportXLS.setOutCell(sheet, 25, row_nr, u'Data:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Resultado Confirmado (   )')
+            ExportXLS.setOutCell(sheet, 25, row_nr, u'Resultado Alterado (   )')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, 0, row_nr, u'Resultado digitado em:')
+            ExportXLS.setOutCell(sheet, 25, row_nr, u'Digitado por:')
+            row_nr += 2
+
+            row = sheet.row(row_nr)
+            for i in range(0, 49):
+                row.write(i, u'-')
+            row_nr += 2
+
+            row_nr = ref_row_nr
+            delta = 25
+
+            ExportXLS.setOutCell(sheet, delta, row_nr, u'Exame Microscópico')
+            row_nr += 2
+
+            ExportXLS.setOutCell(sheet, delta, row_nr, u'Células epiteliais:')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta, row_nr, u'Muco (filamentos):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta, row_nr, u'Cristais:')
+            row_nr += 4
+
+            ExportXLS.setOutCell(sheet, delta, row_nr, u'Leucócitos (/mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta, row_nr, u'Hemácias (/mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta, row_nr, u'Cilindros (/mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Hialinos (/mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Granulosos (/mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Leucocitários (/mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Hemáticos (/mL):')
+            row_nr += 1
+
+            ExportXLS.setOutCell(sheet, delta + 1, row_nr, u'Outros (/mL):')
+            row_nr += 1
 
         return True
