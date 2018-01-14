5af94ba233bd3c3951c2ab2a23d159f7530aceb4
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2018-01-14 17:40:09 -0200

    [IMP] clv_default_jcafb_2018 (IMP wizard/address_document_setup.py)

--- clv_default_jcafb_2018/wizard/address_document_setup.py
+++ clv_default_jcafb_2018/wizard/address_document_setup.py
@@ -24,37 +24,37 @@ from odoo import fields, models
 class AddressDocumentSetup(models.TransientModel):
     _inherit = 'clv.address.document_setup'
 
-    def _default_survey_ids(self):
-        Survey = self.env['survey.survey']
-        survey = Survey.search([
-            ('title', '=', '[QSF18]'),
-        ])
-        survey_id = False
-        if survey.id is not False:
-            survey_id = survey.id
-        return [survey_id]
-    survey_ids = fields.Many2many(
-        comodel_name='survey.survey',
-        relation='clv_address_document_setup_survey_rel',
-        string='Survey Types',
-        default=_default_survey_ids
-    )
+    # def _default_survey_ids(self):
+    #     Survey = self.env['survey.survey']
+    #     survey = Survey.search([
+    #         ('title', '=', '[QSF18]'),
+    #     ])
+    #     survey_id = False
+    #     if survey.id is not False:
+    #         survey_id = survey.id
+    #     return [survey_id]
+    # survey_ids = fields.Many2many(
+    #     comodel_name='survey.survey',
+    #     relation='clv_address_document_setup_survey_rel',
+    #     string='Survey Types',
+    #     default=_default_survey_ids
+    # )
 
-    def _default_category_id(self):
-        DocumentCategory = self.env['clv.document.category']
-        document_category = DocumentCategory.search([
-            ('name', '=', 'Questionário'),
-        ])
-        category_id = False
-        if document_category.id is not False:
-            category_id = document_category.id
-        return category_id
-    category_id = fields.Many2one(
-        comodel_name='clv.document.category',
-        string='Document Category',
-        ondelete='restrict',
-        default=_default_category_id
-    )
+    # def _default_category_id(self):
+    #     DocumentCategory = self.env['clv.document.category']
+    #     document_category = DocumentCategory.search([
+    #         ('name', '=', 'Questionário'),
+    #     ])
+    #     category_id = False
+    #     if document_category.id is not False:
+    #         category_id = document_category.id
+    #     return category_id
+    # category_id = fields.Many2one(
+    #     comodel_name='clv.document.category',
+    #     string='Document Category',
+    #     ondelete='restrict',
+    #     default=_default_category_id
+    # )
 
     def _default_history_marker_id(self):
         HistoryMarker = self.env['clv.history_marker']
