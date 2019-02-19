4785a66aa7484a8955c731e892a6fb072c53fdfe
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-19 20:19:06 -0300

    [ADD] clv_verification_jcafb

---
+++ clv_verification_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Verification (customizations for CLVhealth-PBM Solution)',
+    'summary': 'Verification Module customizations for CLVhealth-PBM Solution.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_verification',
+    ],
+    'data': [
+        'views/verification_menu_view.xml',
+        # 'data/verification.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
