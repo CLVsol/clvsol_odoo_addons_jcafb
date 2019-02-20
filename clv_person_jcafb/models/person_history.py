a0f5ca85385553b41c41dc185c5f29915135e7f9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-02-20 14:46:59 -0300

    [IMP] clv_person_jcafb (IMP lab_test)

--- clv_person_jcafb/models/person_history.py
+++ clv_person_jcafb/models/person_history.py
@@ -26,3 +26,60 @@ class Person(models.Model):
             record.count_documents = len(documents)
             record.count_documents_2 = len(documents_2)
             record.document_ids = [(6, 0, documents.ids)]
+
+    @api.multi
+    def _compute_lab_test_request_ids_and_count(self):
+        for record in self:
+
+            search_domain = [
+                ('ref_id', '=', self._name + ',' + str(record.id)),
+            ]
+            lab_test_requests_2 = self.env['clv.lab_test.request'].search(search_domain)
+
+            if record.phase_id.id is not False:
+                search_domain.append(
+                    ('phase_id.id', '=', record.phase_id.id),
+                )
+            lab_test_requests = self.env['clv.lab_test.request'].search(search_domain)
+
+            record.count_lab_test_requests = len(lab_test_requests)
+            record.count_lab_test_requests_2 = len(lab_test_requests_2)
+            record.lab_test_request_ids = [(6, 0, lab_test_requests.ids)]
+
+    @api.multi
+    def _compute_lab_test_result_ids_and_count(self):
+        for record in self:
+
+            search_domain = [
+                ('ref_id', '=', self._name + ',' + str(record.id)),
+            ]
+            lab_test_results_2 = self.env['clv.lab_test.result'].search(search_domain)
+
+            if record.phase_id.id is not False:
+                search_domain.append(
+                    ('phase_id.id', '=', record.phase_id.id),
+                )
+            lab_test_results = self.env['clv.lab_test.result'].search(search_domain)
+
+            record.count_lab_test_results = len(lab_test_results)
+            record.count_lab_test_results_2 = len(lab_test_results_2)
+            record.lab_test_result_ids = [(6, 0, lab_test_results.ids)]
+
+    @api.multi
+    def _compute_lab_test_report_ids_and_count(self):
+        for record in self:
+
+            search_domain = [
+                ('ref_id', '=', self._name + ',' + str(record.id)),
+            ]
+            lab_test_reports_2 = self.env['clv.lab_test.report'].search(search_domain)
+
+            if record.phase_id.id is not False:
+                search_domain.append(
+                    ('phase_id.id', '=', record.phase_id.id),
+                )
+            lab_test_reports = self.env['clv.lab_test.report'].search(search_domain)
+
+            record.count_lab_test_reports = len(lab_test_reports)
+            record.count_lab_test_reports_2 = len(lab_test_reports_2)
+            record.lab_test_report_ids = [(6, 0, lab_test_reports.ids)]
