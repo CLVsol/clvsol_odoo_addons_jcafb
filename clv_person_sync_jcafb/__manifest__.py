15030c6faa2d8e769ab8725c48a58d2785a4d7c9
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-06 20:57:12 -0300

    [IMP] clv_person_sync_jcafb (IMP data/person_sync.xml)

--- clv_person_sync_jcafb/__manifest__.py
+++ clv_person_sync_jcafb/__manifest__.py
@@ -11,7 +11,7 @@
     'license': 'AGPL-3',
     'website': 'https://github.com/CLVsol',
     'depends': [
-        'clv_person',
+        'clv_person_jcafb',
         'clv_external_sync',
     ],
     'data': [
