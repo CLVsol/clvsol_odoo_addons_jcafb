0e7b0794e3a6dcd8ef2f9c036b5db959983aa62d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-12 15:16:13 -0300

    [IMP] clv_address_jcafb (ADD address_code)

--- clv_address_jcafb/__manifest__.py
+++ clv_address_jcafb/__manifest__.py
@@ -21,10 +21,12 @@
         'clv_employee',
     ],
     'data': [
+        'data/address_seq.xml',
         'data/document.xml',
         'data/community_member.xml',
         'data/event_attendee.xml',
         'data/lab_test.xml',
+        'views/address_code_view.xml',
         'views/document_view.xml',
         'views/community_member_view.xml',
         'views/event_attendee_view.xml',
