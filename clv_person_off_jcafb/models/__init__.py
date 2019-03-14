c45611eed50e3ffb7c0157eb05fb1179fba31343
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-03-14 14:19:34 -0300

    [IMP] clv_person_off_jcafb (REM *_code_off)

--- clv_person_off_jcafb/models/__init__.py
+++ clv_person_off_jcafb/models/__init__.py
@@ -7,8 +7,3 @@ from . import community_member
 from . import event_attendee
 from . import lab_test
 from . import person_off_reg_state
-from . import person_code_off
-from . import document_code_off
-from . import lab_test_request_code_off
-from . import lab_test_result_code_off
-from . import lab_test_report_code_off
