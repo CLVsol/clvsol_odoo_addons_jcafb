45ef3f657e6ef08251d5354b145c165067c6dcc0
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-12 17:45:05 -0300

    [ADD] clv_data_processing_jcafb

---
+++ clv_data_processing_jcafb/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Data Processing (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'Data Processing Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_data_processing',
+    ],
+    'data': [
+        'views/data_processing_menu_view.xml',
+        # 'data/data_processing.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
