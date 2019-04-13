c75f9d6b2d49ebf12a6d0bf750a2453c83185f7c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-13 17:54:57 -0300

    [IMP] clv_base_jcafb (renamed: views/data_processing_menu_view.xml -> views/processing_menu_view.xml)

--- clv_base_jcafb/__manifest__.py
+++ clv_base_jcafb/__manifest__.py
@@ -25,7 +25,7 @@
         'views/export_menu_view.xml',
         'views/external_sync_menu_view.xml',
         'views/verification_menu_view.xml',
-        'views/data_processing_menu_view.xml',
+        'views/processing_menu_view.xml',
         'data/annotation_seq.xml',
         'data/global_settings_filestore.xml',
     ],
