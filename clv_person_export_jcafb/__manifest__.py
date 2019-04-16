3ee492a216f6989d8e9c149c25cea00fbe4fb5e6
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-04-16 20:32:14 -0300

    [ADD] clv_person_export_jcafb

---
+++ clv_person_export_jcafb/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Person Export (customizations for CLVhealth-JCAFB Solution)',
+    'summary': 'Person Export Module customizations for CLVhealth-JCAFB Solution.',
+    'version': '4.0.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'depends': [
+        'clv_person_jcafb',
+        'clv_export',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
