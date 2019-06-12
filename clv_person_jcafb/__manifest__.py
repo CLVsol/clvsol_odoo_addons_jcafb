fb3bcc8bef4f5f67f493bc9bec871fadf06c3606
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-06-12 15:24:28 -0300

    [IMP] clv_person_jcafb (ADD person)

--- clv_person_jcafb/__manifest__.py
+++ clv_person_jcafb/__manifest__.py
@@ -25,6 +25,7 @@
         'data/community_member.xml',
         'data/event_attendee.xml',
         'data/lab_test.xml',
+        'views/person_view.xml',
         'views/person_code_view.xml',
         'views/document_view.xml',
         'views/community_member_view.xml',
