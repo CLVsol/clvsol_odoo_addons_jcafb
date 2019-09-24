2f692952b243ff1a51332d7a7223804320ff0b03
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-09-24 16:11:51 -0300

    [IMP] clv_person_aux_verification_jcafb

--- clv_person_aux_verification_jcafb/models/verification_outcome.py
+++ clv_person_aux_verification_jcafb/models/verification_outcome.py
@@ -2,7 +2,11 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
+
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class PersonAux(models.Model):
@@ -36,3 +40,14 @@ class PersonAux(models.Model):
             record.count_verification_outcomes = len(verification_outcomes)
             record.count_verification_outcomes_2 = len(verification_outcomes)
             record.verification_outcome_ids = [(6, 0, verification_outcomes.ids)]
+
+
+class VerificationOutcome(models.Model):
+    _inherit = 'clv.verification.outcome'
+
+    def _person_aux_verification(self, verification_outcome, model_object):
+
+        _logger.info(u'%s %s', '>>>>>>>>>>>>>>> (model_object):', model_object.name)
+
+        verification_outcome.outcome_text = 'Executed "_person_aux_verification"'
+        # verification_outcome.state = 'ok'
