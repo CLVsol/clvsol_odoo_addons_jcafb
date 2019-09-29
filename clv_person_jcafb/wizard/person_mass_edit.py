ee57aa5b1aa933a8e7f29b2986a7c1b743269a30
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-09-29 16:49:11 -0300

    [IMP] clv_person_jcafb (IMP wizard/person_mass_edit)

--- clv_person_jcafb/wizard/person_mass_edit.py
+++ clv_person_jcafb/wizard/person_mass_edit.py
@@ -12,6 +12,33 @@ _logger = logging.getLogger(__name__)
 class PersonMassEdit(models.TransientModel):
     _inherit = 'clv.person.mass_edit'
 
+    reg_state = fields.Selection(
+        [('draft', 'Draft'),
+         ('revised', 'Revised'),
+         ('done', 'Done'),
+         ('canceled', 'Canceled')
+         ], string='Register State', default=False, readonly=False, required=False
+    )
+    reg_state_selection = fields.Selection(
+        [('set', 'Set'),
+         ], string='Register State:', default=False, readonly=False, required=False
+    )
+
+    state = fields.Selection(
+        [('new', 'New'),
+         ('available', 'Available'),
+         ('waiting', 'Waiting'),
+         ('selected', 'Selected'),
+         ('unselected', 'Unselected'),
+         ('unavailable', 'Unavailable'),
+         ('unknown', 'Unknown')
+         ], string='State', default=False, readonly=False, required=False
+    )
+    state_selection = fields.Selection(
+        [('set', 'Set'),
+         ], string='State:', default=False, readonly=False, required=False
+    )
+
     random_field = fields.Char(
         string='Random ID', default=False,
         help='Use "/" to get an automatic new Random ID.'
@@ -31,6 +58,16 @@ class PersonMassEdit(models.TransientModel):
         for person in self.person_ids:
 
             _logger.info(u'%s %s', '>>>>>', person.name)
+
+            if self.reg_state_selection == 'set':
+                person.reg_state = self.reg_state
+            if self.reg_state_selection == 'remove':
+                person.reg_state = False
+
+            if self.state_selection == 'set':
+                person.state = self.state
+            if self.state_selection == 'remove':
+                person.state = False
 
             if self.random_field_selection == 'set':
                 person.random_field = self.random_field
