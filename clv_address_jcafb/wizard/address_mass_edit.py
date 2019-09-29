e596266fa1ba5340a779e5e22045a53ec03adb7b
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-09-29 16:11:46 -0300

    [IMP] clv_address_jcafb (IMP wizard/address_mass_edit)

--- clv_address_jcafb/wizard/address_mass_edit.py
+++ clv_address_jcafb/wizard/address_mass_edit.py
@@ -12,6 +12,33 @@ _logger = logging.getLogger(__name__)
 class AddressMassEdit(models.TransientModel):
     _inherit = 'clv.address.mass_edit'
 
+    reg_state = fields.Selection(
+        [('draft', 'Draft'),
+         ('revised', 'Revised'),
+         ('done', 'Done'),
+         ('canceled', 'Canceled')
+         ], string='Register State', default=False, readonly=False, required=False
+    )
+    reg_state_selection = fields.Selection(
+        [('set', 'Set'),
+         ], string='Register State:', default=False, readonly=False, required=False
+    )
+
+    state = fields.Selection(
+        [('new', 'New'),
+         ('available', 'Available'),
+         ('waiting', 'Waiting'),
+         ('selected', 'Selected'),
+         ('unselected', 'Unselected'),
+         ('unavailable', 'Unavailable'),
+         ('unknown', 'Unknown')
+         ], string='State', default=False, readonly=False, required=False
+    )
+    state_selection = fields.Selection(
+        [('set', 'Set'),
+         ], string='State:', default=False, readonly=False, required=False
+    )
+
     employee_id = fields.Many2one(
         comodel_name='hr.employee',
         string='Responsible Empĺoyee'
@@ -31,6 +58,16 @@ class AddressMassEdit(models.TransientModel):
         for address in self.address_ids:
 
             _logger.info(u'%s %s', '>>>>>', address.name)
+
+            if self.reg_state_selection == 'set':
+                address.reg_state = self.reg_state
+            if self.reg_state_selection == 'remove':
+                address.reg_state = False
+
+            if self.state_selection == 'set':
+                address.state = self.state
+            if self.state_selection == 'remove':
+                address.state = False
 
             if self.employee_id_selection == 'set':
                 address.employee_id = self.employee_id
