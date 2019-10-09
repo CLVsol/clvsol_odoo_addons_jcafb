cf7a0fb0096bd8a84d528bd6e7b14d13e46402a7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-10-09 16:51:00 -0300

    [IMP] clv_family_aux_verification_jcafb (ADD wizard/family_aux_mass_edit)

--- clv_family_aux_verification_jcafb/__manifest__.py
+++ clv_family_aux_verification_jcafb/__manifest__.py
@@ -17,6 +17,7 @@
     'data': [
         'data/family_aux_verification.xml',
         'views/verification_outcome_view.xml',
+        'wizard/family_aux_mass_edit_view.xml',
         'wizard/family_aux_verification_exec_view.xml',
     ],
     'demo': [],
