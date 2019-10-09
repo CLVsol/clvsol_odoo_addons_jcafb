ed78229fd18a3f0466ffd66becf2d7b2e9214818
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-10-09 16:53:49 -0300

    [IMP] clv_person_verification_jcafb (ADD wizard/person_mass_edit)

---
+++ clv_person_verification_jcafb/wizard/person_mass_edit.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import logging
+
+from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
+
+
+class PersonMassEdit(models.TransientModel):
+    _inherit = 'clv.person.mass_edit'
+
+    verification_marker_ids = fields.Many2many(
+        comodel_name='clv.verification.marker',
+        relation='clv_person_mass_edit_verification_marker_rel',
+        column1='person_id',
+        column2='verification_marker_id',
+        string='Verification Markers'
+    )
+    verification_marker_ids_selection = fields.Selection(
+        [('add', 'Add'),
+         ('remove_m2m', 'Remove'),
+         ('set', 'Set'),
+         ], string='Verification Markers:', default=False, readonly=False, required=False
+    )
+
+    @api.multi
+    def do_person_mass_edit(self):
+        self.ensure_one()
+
+        super().do_person_mass_edit()
+
+        for person in self.person_ids:
+
+            _logger.info(u'%s %s', '>>>>>', person.name)
+
+            if self.verification_marker_ids_selection == 'add':
+                m2m_list = []
+                for verification_marker_id in self.verification_marker_ids:
+                    m2m_list.append((4, verification_marker_id.id))
+                _logger.info(u'%s %s', '>>>>>>>>>>', m2m_list)
+                person.verification_marker_ids = m2m_list
+            if self.verification_marker_ids_selection == 'remove_m2m':
+                m2m_list = []
+                for verification_marker_id in self.verification_marker_ids:
+                    m2m_list.append((3, verification_marker_id.id))
+                _logger.info(u'%s %s', '>>>>>>>>>>', m2m_list)
+                person.verification_marker_ids = m2m_list
+            if self.verification_marker_ids_selection == 'set':
+                m2m_list = []
+                for verification_marker_id in person.verification_marker_ids:
+                    m2m_list.append((3, verification_marker_id.id))
+                _logger.info(u'%s %s', '>>>>>>>>>>', m2m_list)
+                person.verification_marker_ids = m2m_list
+                m2m_list = []
+                for verification_marker_id in self.verification_marker_ids:
+                    m2m_list.append((4, verification_marker_id.id))
+                _logger.info(u'%s %s', '>>>>>>>>>>', m2m_list)
+                person.verification_marker_ids = m2m_list
+
+        return True
