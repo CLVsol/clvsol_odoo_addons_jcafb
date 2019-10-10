cbedbd470ad27262362882e80f7533d32d0af65d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-10-10 14:22:37 -0300

    [IMP] clv_person_aux_verification_jcafb (IMP models/verification_outcome.py)

--- clv_person_aux_verification_jcafb/models/verification_outcome.py
+++ clv_person_aux_verification_jcafb/models/verification_outcome.py
@@ -189,10 +189,10 @@ class VerificationOutcome(models.Model):
                 outcome_info += _('"Date of Birth" is missing.\n')
                 state = self._get_verification_outcome_state(state, 'Warning (L0)')
 
-        if model_object.phase_id.id is False:
+            if model_object.phase_id.id is False:
 
-            outcome_info += _('"Phase" is missing.\n')
-            state = self._get_verification_outcome_state(state, 'Error (L0)')
+                outcome_info += _('"Phase" is missing.\n')
+                state = self._get_verification_outcome_state(state, 'Error (L0)')
 
         if outcome_info == '':
             outcome_info = False
