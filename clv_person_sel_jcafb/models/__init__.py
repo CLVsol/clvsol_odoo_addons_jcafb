91dd9e8dfe1dee0f63e7e3ea8dd6543c37ef1991
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-11-04 20:50:04 -0300

    [IMP] clv_person_sel_jcafb (ADD abstract_row)

--- clv_person_sel_jcafb/models/__init__.py
+++ clv_person_sel_jcafb/models/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import abstract_row
 from . import person_sel_age_group
 from . import person_sel_district
 from . import person_sel_group
