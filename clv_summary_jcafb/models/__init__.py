0821e5019ad67641ff0e120d8a169a706119626a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-11-23 21:10:28 -0300

    [IMP] clv_summary_jcafb (ADD summary_address)

--- clv_summary_jcafb/models/__init__.py
+++ clv_summary_jcafb/models/__init__.py
@@ -5,5 +5,6 @@
 from . import summary_document
 from . import summary_lab_test_request
 from . import summary_event
+from . import summary_address
 from . import summary_family
 from . import summary_person
