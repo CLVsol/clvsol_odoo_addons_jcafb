ee26efb4185bfb52b6812c9eb52d9918a0872d1a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-12-09 15:32:37 -0300

    [IMP] clv_document_jcafb (IMP survey)

--- clv_document_jcafb/models/survey.py
+++ clv_document_jcafb/models/survey.py
@@ -19,3 +19,34 @@ class Document(models.Model):
         comodel_name='survey.user_input',
         string='Base Survey User Input'
     )
+
+
+class SurveyUserInput(models.Model):
+    _inherit = 'survey.user_input'
+
+    document_code = fields.Char(
+        string='Document Code',
+        readonly=True
+    )
+
+    person_code = fields.Char(
+        string='Person Code',
+        readonly=True
+    )
+
+    family_code = fields.Char(
+        string='Family Code',
+        readonly=True
+    )
+
+    address_code = fields.Char(
+        string='Address Code',
+        readonly=True
+    )
+
+    document_id = fields.Many2one(
+        comodel_name='clv.document',
+        string='Related Document'
+    )
+
+    notes = fields.Text(string='Notes')
