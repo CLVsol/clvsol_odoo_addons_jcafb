cc2932ddc87b892dea3b223096c218f26358cf98
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2019-12-27 20:32:44 -0300

    [IMP] clv_person_aux_jcafb (ADD wizard/person_aux_event_setup)

--- clv_person_aux_jcafb/wizard/__init__.py
+++ clv_person_aux_jcafb/wizard/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+from . import person_aux_event_setup
 from . import person_aux_document_setup
 from . import person_aux_lab_test_request_setup
 from . import person_aux_mass_edit
