3febd9ac74d0676e24f39cb31ff6d3d1c8e98a6e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-01-12 16:37:04 -0300

    [IMP] clv_document_jcafb (IMP wizard/survey_user_input_refresh.py)

--- clv_document_jcafb/wizard/survey_user_input_refresh.py
+++ clv_document_jcafb/wizard/survey_user_input_refresh.py
@@ -3,10 +3,35 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import logging
+import re
 
 from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
+
+
+def format_code(code):
+    code = re.sub("\D", "", code)
+    code_len = len(code) - 2
+    while len(code) < 16:
+        code = '0' + code
+    code_str = "%s.%s.%s.%s.%s-%s" % (str(code[0]) + str(code[1]),
+                                      str(code[2]) + str(code[3]) + str(code[4]),
+                                      str(code[5]) + str(code[6]) + str(code[7]),
+                                      str(code[8]) + str(code[9]) + str(code[10]),
+                                      str(code[11]) + str(code[12]) + str(code[13]),
+                                      str(code[14]) + str(code[15]))
+    if code_len <= 3:
+        code_form = code_str[18 - code_len:21]
+    elif code_len > 3 and code_len <= 6:
+        code_form = code_str[17 - code_len:21]
+    elif code_len > 6 and code_len <= 9:
+        code_form = code_str[16 - code_len:21]
+    elif code_len > 9 and code_len <= 12:
+        code_form = code_str[15 - code_len:21]
+    elif code_len > 12 and code_len <= 14:
+        code_form = code_str[14 - code_len:21]
+    return code_form
 
 
 class SurveyUserInputRefresh(models.TransientModel):
@@ -69,19 +94,19 @@ class SurveyUserInputRefresh(models.TransientModel):
                     # _logger.info(u'%s %s', '>>>>> (value_text):', value_text)
 
                     if question_parameter == 'document_code':
-                        document_code = value_text
+                        document_code = format_code(value_text)
                         survey_user_input.document_code = document_code
 
                     if question_parameter == 'person_code':
-                        person_code = value_text
+                        person_code = format_code(value_text)
                         survey_user_input.person_code = person_code
 
                     if question_parameter == 'family_code':
-                        family_code = value_text
+                        family_code = format_code(value_text)
                         survey_user_input.family_code = family_code
 
                     if question_parameter == 'address_code':
-                        address_code = value_text
+                        address_code = format_code(value_text)
                         survey_user_input.address_code = address_code
 
                 if document_code is not False:
