c04254af7974f88d103207ff41d3a5e0f30a296e
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-01-20 15:34:24 -0300

    [IMP] clv_summary_jcafb (ADD summary_lab_test_result)

--- clv_summary_jcafb/models/__init__.py
+++ clv_summary_jcafb/models/__init__.py
@@ -4,6 +4,7 @@
 
 from . import summary_document
 from . import summary_lab_test_request
+from . import summary_lab_test_result
 from . import summary_event
 from . import summary_address
 from . import summary_family
