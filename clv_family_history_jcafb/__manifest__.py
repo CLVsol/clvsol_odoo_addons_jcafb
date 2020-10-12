0bc12a9d40e8096b5234058ed923c8a1d286103f
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-12 15:58:10 -0300

    [IMP] clv_family_history_jcafb (REM family_history_reg_state)

--- clv_family_history_jcafb/__manifest__.py
+++ clv_family_history_jcafb/__manifest__.py
@@ -17,7 +17,6 @@
     'data': [
         'security/ir.model.access.csv',
         'views/employee_view.xml',
-        'views/family_history_reg_state_view.xml',
         'views/family_history_state_view.xml',
         'wizard/family_history_updt_view.xml',
     ],
