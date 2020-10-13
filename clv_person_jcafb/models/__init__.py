5cb100b757bb566159bc662dfddfbc67f9c5bb12
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-13 16:02:38 -0300

    [REF] clv_person_jcafb

--- clv_person_jcafb/models/__init__.py
+++ clv_person_jcafb/models/__init__.py
@@ -2,13 +2,9 @@
 # Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import person
 from . import person_code
 from . import document
 from . import community_member
 from . import set_element
 from . import event_attendee
 from . import lab_test
-from . import person_state
-from . import address
-from . import family
