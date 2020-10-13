fd58285fd2700f083fe13c14f7edc336d934b26d
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-13 16:02:16 -0300

    [REF] clv_person_history_jcafb

--- clv_person_history_jcafb/__manifest__.py
+++ clv_person_history_jcafb/__manifest__.py
@@ -16,8 +16,6 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/employee_view.xml',
-        'views/person_history_state_view.xml',
         'wizard/person_history_updt_view.xml',
         'wizard/person_history_person_associate_to_set_view.xml',
         'wizard/person_history_person_mass_edit_view.xml',
