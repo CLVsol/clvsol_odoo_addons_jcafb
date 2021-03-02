203367b43bb89adb91c5f08898862c11b06bbdbe
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-03-02 18:48:03 -0300

    [IMP] clv_base_jcafb (REM views/export_menu_view.xml)

--- clv_base_jcafb/__manifest__.py
+++ clv_base_jcafb/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Base Module (customizations for CLVhealth-JCAFB Solution)',
     'summary': 'Base Module Module customizations for CLVhealth-JCAFB Solution.',
-    'version': '12.0.4.0',
+    'version': '14.0.5.0',
     'author': 'Carlos Eduardo Vercelino - CLVsol',
     'category': 'CLVsol Solutions',
     'license': 'AGPL-3',
@@ -15,7 +15,6 @@
     ],
     'data': [
         # 'views/mfile_menu_view.xml',
-        # 'views/export_menu_view.xml',
         # 'views/report_menu_view.xml',
         # 'data/annotation_seq.xml',
         'data/global_settings_filestore.xml',
