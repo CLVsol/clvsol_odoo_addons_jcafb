b0e7d74d765bd5833dcad72f12355aeba5a49be8
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-07-25 15:27:02 -0300

    [IMP] clv_patient_community_jcafb (ADD wizard/lab_test_result_associate_from_person_to_patient)

--- clv_patient_community_jcafb/__manifest__.py
+++ clv_patient_community_jcafb/__manifest__.py
@@ -22,6 +22,7 @@
         'security/ir.model.access.csv',
         'wizard/document_associate_from_person_to_patient_view.xml',
         'wizard/lab_test_request_associate_from_person_to_patient_view.xml',
+        'wizard/lab_test_result_associate_from_person_to_patient_view.xml',
         'wizard/event_attendee_associate_from_person_to_patient_view.xml',
     ],
     'demo': [],
--- clv_patient_community_jcafb/wizard/__init__.py
+++ clv_patient_community_jcafb/wizard/__init__.py
@@ -3,5 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import document_associate_from_person_to_patient
-from . import event_attendee_associate_from_person_to_patient
 from . import lab_test_request_associate_from_person_to_patient
+from . import lab_test_result_associate_from_person_to_patient
+from . import event_attendee_associate_from_person_to_patient
