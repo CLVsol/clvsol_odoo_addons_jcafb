3599aad8256a3d06e9a64b9b13de597e96ce6266
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-07-25 14:34:02 -0300

    [IMP] clv_residence_community_jcafb (ADD wizard/lab_test_result_associate_from_address_to_residence)

--- clv_residence_community_jcafb/__manifest__.py
+++ clv_residence_community_jcafb/__manifest__.py
@@ -24,6 +24,7 @@
         'wizard/document_associate_from_address_to_residence_view.xml',
         'wizard/document_associate_from_family_to_residence_view.xml',
         'wizard/lab_test_request_associate_from_address_to_residence_view.xml',
+        'wizard/lab_test_result_associate_from_address_to_residence_view.xml',
     ],
     'demo': [],
     'test': [],
