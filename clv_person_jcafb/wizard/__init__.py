1d42bcbee520b927f034cbecac817483bb56be68
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-08-09 11:22:03 -0300

    IMP] clv_person_jcafb (ADD wizard/document_related_address_set_marker)

--- clv_person_jcafb/wizard/__init__.py
+++ clv_person_jcafb/wizard/__init__.py
@@ -5,4 +5,5 @@
 from . import person_associate_to_set
 from . import person_document_setup
 from . import person_lab_test_request_setup
+from . import document_related_address_set_marker
 from . import document_related_person_set_marker
