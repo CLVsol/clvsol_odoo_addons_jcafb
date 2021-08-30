b77c33413798d13ed4cb50d953a34660f640cb64
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-08-30 09:07:24 -0300

    [IMP] clv_document_jcafb (REM wizard/survey_user_input_validate)

--- clv_document_jcafb/wizard/__init__.py
+++ clv_document_jcafb/wizard/__init__.py
@@ -6,5 +6,4 @@ from . import document_type_items_setup
 from . import document_items_updt_from_survey
 # from . import document_type_setup
 from . import survey_user_input_refresh
-from . import survey_user_input_validate
 from . import document_set_survey_user_input
