cca91b4dea829b743ba6e63190213132fd3a5de7
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-09-17 13:19:58 -0300

    [IMP] clv_lab_test_jcafb (ADD wizard/lab_test_result_export_xls)

--- clv_lab_test_jcafb/wizard/__init__.py
+++ clv_lab_test_jcafb/wizard/__init__.py
@@ -6,6 +6,7 @@ from . import lab_test_request_receive
 from . import lab_test_result_approve
 from . import lab_test_report_approve
 from . import lab_test_report_export_xls
+from . import lab_test_result_export_xls
 from . import lab_test_request_associate_to_set
 from . import lab_test_report_associate_to_set
 from . import lab_test_result_set_survey_user_input
