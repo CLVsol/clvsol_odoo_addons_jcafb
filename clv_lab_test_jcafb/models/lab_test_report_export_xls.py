16c59772c149dbfd97ecabf7becb7d13f549edb3
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-09-21 14:56:11 -0300

    [IMP] clv_lab_test_jcafb (IMP models/lab_test_report_export_xls.py)

--- clv_lab_test_jcafb/models/lab_test_report_export_xls.py
+++ clv_lab_test_jcafb/models/lab_test_report_export_xls.py
@@ -8,10 +8,10 @@ from xlutils.copy import copy
 from xlrd import open_workbook
 from datetime import datetime
 
-from shutil import copyfile
+# from shutil import copyfile
 
-import os.path
-from PIL import Image
+# import os.path
+# from PIL import Image
 
 from odoo import models
 
@@ -48,12 +48,12 @@ class LabTestReport(models.Model):
             wbook = xlwt.Workbook()
             sheet = wbook.add_sheet(file_name)
 
-        logo_file_path = template_dir_path + '/' + 'jcafb.bmp'
-        if not os.path.isfile(logo_file_path):
-            img = Image.open(template_dir_path + '/' + 'jcafb.png')
-            r, g, b, a = img.split()
-            img = Image.merge('RGB', (r, g, b))
-            img.save(logo_file_path)
+        lab_test_type_id = self.lab_test_type_id.id
+        reference_name = self.ref_id.name
+        reference_code = self.ref_id.code
+        professional_name = self.employee_id.name
+        professional_id = self.employee_id.professional_id
+        date_approved = datetime.strftime(self.date_approved, '%d-%m-%Y')
 
         _logger.info(u'%s %s %s %s', '>>>>>>>>>>', lab_test_request_code, lab_test_type, use_template)
 
@@ -61,37 +61,86 @@ class LabTestReport(models.Model):
 
         ExportXLS = self.env['clv.export_xls']
 
-        sheet.insert_bitmap(logo_file_path, 0, 3)
+        LabTestTypeExportXlsParam = self.env['clv.lab_test.export_xls.param']
 
-        ExportXLS.setOutCell(sheet, 6, 7, self.ref_id.name)
-        ExportXLS.setOutCell(sheet, 35, 7, self.ref_id.code)
-        date = datetime.strftime(self.date_approved, '%d-%m-%Y')
-        ExportXLS.setOutCell(sheet, 9, 9, date)
-        ExportXLS.setOutCell(sheet, 38, 9, lab_test_request_code)
+        parameters = LabTestTypeExportXlsParam.search([
+            ('lab_test_type_id', '=', lab_test_type_id),
+            ('display', '=', 'report'),
+            ('parameter_type', '=', 'image_file_name'),
+        ])
 
-        result = self.criterion_ids.search([
-            ('lab_test_result_id', '=', self.lab_test_result_id.id),
-            ('code', '=', 'EAN21_03_02'),
-        ]).result
-        if result is not False:
-            ExportXLS.setOutCell(sheet, 16, 23, result)
+        for parameter in parameters:
 
-        result_peso = self.criterion_ids.search([
-            ('lab_test_result_id', '=', self.lab_test_result_id.id),
-            ('code', '=', 'EAN21_02_01'),
-        ]).result
-        if result_peso is not False:
-            ExportXLS.setOutCell(sheet, 5, 29, result_peso)
+            # image_file_path = template_dir_path + '/' + 'jcafb.bmp'
+            image_file_path = template_dir_path + '/' + parameter.parameter
+            # if not os.path.isfile(image_file_path):
+            #     img = Image.open(template_dir_path + '/' + 'jcafb.png')
+            #     r, g, b, a = img.split()
+            #     img = Image.merge('RGB', (r, g, b))
+            #     img.save(image_file_path)
+            sheet.insert_bitmap(image_file_path, 0, 3)
 
-        result_altura = self.criterion_ids.search([
-            ('lab_test_result_id', '=', self.lab_test_result_id.id),
-            ('code', '=', 'EAN21_02_03'),
-        ]).result
-        if result_altura is not False:
-            ExportXLS.setOutCell(sheet, 18, 29, result_altura)
+        parameters = LabTestTypeExportXlsParam.search([
+            ('lab_test_type_id', '=', lab_test_type_id),
+            ('display', '=', 'report'),
+            ('parameter_type', '=', 'variable_name'),
+        ])
 
-        ExportXLS.setOutCell(sheet, 33, 57, self.employee_id.name)
-        ExportXLS.setOutCell(sheet, 36, 58, self.employee_id.professional_id)
+        for parameter in parameters:
+
+            # ExportXLS.setOutCell(sheet, 6, 7, reference_name)
+            # ExportXLS.setOutCell(sheet, 35, 7, reference_code)
+            # ExportXLS.setOutCell(sheet, 9, 9, date_approved)
+            # ExportXLS.setOutCell(sheet, 38, 9, lab_test_request_code)
+            # ExportXLS.setOutCell(sheet, 33, 57, professional_name)
+            # ExportXLS.setOutCell(sheet, 36, 58, professional_id)
+            ExportXLS.setOutCell(sheet, parameter.col_nr, parameter.row_nr, eval(parameter.parameter))
+
+        parameters = LabTestTypeExportXlsParam.search([
+            ('lab_test_type_id', '=', lab_test_type_id),
+            ('display', '=', 'report'),
+            ('parameter_type', '=', 'expression'),
+        ])
+
+        for parameter in parameters:
+
+            ExportXLS.setOutCell(sheet, parameter.col_nr, parameter.row_nr, eval(parameter.parameter))
+
+        parameters = LabTestTypeExportXlsParam.search([
+            ('lab_test_type_id', '=', lab_test_type_id),
+            ('display', '=', 'report'),
+            ('parameter_type', '=', 'result_code'),
+        ])
+
+        for parameter in parameters:
+
+            # result = self.criterion_ids.search([
+            #     ('lab_test_result_id', '=', self.lab_test_result_id.id),
+            #     ('code', '=', 'EAN21_03_02'),
+            # ]).result
+            # if result_peso is not False:
+            #     ExportXLS.setOutCell(sheet, 16, 23, result)
+
+            # result_peso = self.criterion_ids.search([
+            #     ('lab_test_result_id', '=', self.lab_test_result_id.id),
+            #     ('code', '=', 'EAN21_02_01'),
+            # ]).result
+            # if result_peso is not False:
+            #     ExportXLS.setOutCell(sheet, 5, 29, result_peso)
+
+            # result_altura = self.criterion_ids.search([
+            #     ('lab_test_result_id', '=', self.lab_test_result_id.id),
+            #     ('code', '=', 'EAN21_02_03'),
+            # ]).result
+            # if result_altura is not False:
+            #     ExportXLS.setOutCell(sheet, 18, 29, result_altura)
+
+            result = self.criterion_ids.search([
+                ('lab_test_result_id', '=', self.lab_test_result_id.id),
+                ('code', '=', parameter.parameter),
+            ]).result
+            if result is not False:
+                ExportXLS.setOutCell(sheet, parameter.col_nr, parameter.row_nr, result)
 
         save_book = True
 
