9042af0331680ffb02ac2816789140541cb15472
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2021-09-21 14:57:02 -0300

    [IMP] clv_lab_test_jcafb (IMP models/lab_test_result_export_xls.py)

--- clv_lab_test_jcafb/models/lab_test_result_export_xls.py
+++ clv_lab_test_jcafb/models/lab_test_result_export_xls.py
@@ -50,33 +50,66 @@ class LabTestResult(models.Model):
             wbook = xlwt.Workbook()
             sheet = wbook.add_sheet(file_name)
 
-        logo_file_path = template_dir_path + '/' + 'jcafb.bmp'
-        if not os.path.isfile(logo_file_path):
-            img = Image.open(template_dir_path + '/' + 'jcafb.png')
-            r, g, b, a = img.split()
-            img = Image.merge('RGB', (r, g, b))
-            img.save(logo_file_path)
+        delta_hours = -3
+
+        lab_test_type_id = self.lab_test_type_id.id
+        reference_name = self.ref_id.name
+        reference_code = self.ref_id.code
+        received_by_name = self.lab_test_request_id.employee_id.name
+        received_by_code = self.lab_test_request_id.employee_id.code
+        date_received = (self.lab_test_request_id.date_received + timedelta(hours=delta_hours)).strftime('%d-%m-%Y  %H:%M:%S')
 
         _logger.info(u'%s %s %s %s', '>>>>>>>>>>', lab_test_request_code, lab_test_type, use_template)
 
         save_book = False
 
-        delta_hours = -3
-
         ExportXLS = self.env['clv.export_xls']
 
-        ExportXLS.setOutCell(sheet, 11, 3, lab_test_request_code)
-        ExportXLS.setOutCell(sheet, 35, 3, lab_test_result_code)
+        LabTestTypeExportXlsParam = self.env['clv.lab_test.export_xls.param']
 
-        ExportXLS.setOutCell(sheet, 11, 5, self.ref_id.name)
-        ExportXLS.setOutCell(sheet, 11, 7, self.ref_id.code)
+        parameters = LabTestTypeExportXlsParam.search([
+            ('lab_test_type_id', '=', lab_test_type_id),
+            ('display', '=', 'result'),
+            ('parameter_type', '=', 'image_file_name'),
+        ])
 
-        ExportXLS.setOutCell(sheet, 11, 9, self.lab_test_request_id.employee_id.name)
-        ExportXLS.setOutCell(sheet, 11, 10, self.lab_test_request_id.employee_id.code)
-        ExportXLS.setOutCell(
-            sheet, 11, 11,
-            (self.lab_test_request_id.date_received + timedelta(hours=delta_hours)).strftime('%d-%m-%Y  %H:%M:%S')
-        )
+        for parameter in parameters:
+
+            # image_file_path = template_dir_path + '/' + 'jcafb.bmp'
+            image_file_path = template_dir_path + '/' + parameter.parameter
+            # if not os.path.isfile(image_file_path):
+            #     img = Image.open(template_dir_path + '/' + 'jcafb.png')
+            #     r, g, b, a = img.split()
+            #     img = Image.merge('RGB', (r, g, b))
+            #     img.save(image_file_path)
+            sheet.insert_bitmap(image_file_path, 0, 3)
+
+        parameters = LabTestTypeExportXlsParam.search([
+            ('lab_test_type_id', '=', lab_test_type_id),
+            ('display', '=', 'result'),
+            ('parameter_type', '=', 'variable_name'),
+        ])
+
+        for parameter in parameters:
+
+            # ExportXLS.setOutCell(sheet, 11, 3, lab_test_request_code)
+            # ExportXLS.setOutCell(sheet, 35, 3, lab_test_result_code)
+            # ExportXLS.setOutCell(sheet, 11, 5, reference_name)
+            # ExportXLS.setOutCell(sheet, 11, 7, reference_code)
+            # ExportXLS.setOutCell(sheet, 11, 9, received_by_name)
+            # ExportXLS.setOutCell(sheet, 11, 10, received_by_code)
+            # ExportXLS.setOutCell(sheet, 11, 11, date_received)
+            ExportXLS.setOutCell(sheet, parameter.col_nr, parameter.row_nr, eval(parameter.parameter))
+
+        parameters = LabTestTypeExportXlsParam.search([
+            ('lab_test_type_id', '=', lab_test_type_id),
+            ('display', '=', 'result'),
+            ('parameter_type', '=', 'expression'),
+        ])
+
+        for parameter in parameters:
+
+            ExportXLS.setOutCell(sheet, parameter.col_nr, parameter.row_nr, eval(parameter.parameter))
 
         save_book = True
 
